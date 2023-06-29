ca47a735f2184220db683a74dc990af881c6090d
Thadshajini Paramsothy <thadshap@ntnu.no>
2023-06-29 13:48:08 +0200

    added more stuff

--- DotNetTest/Controllers/WeatherForecastController.cs
+++ DotNetTest/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace DotNetTest.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -29,5 +29,18 @@ namespace DotNetTest.Controllers
             })
             .ToArray();
         }
+
+        [HttpDelete("GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get1()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
     }
 }
